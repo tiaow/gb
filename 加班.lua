cb4daa3f3b56ec27cae79640da8f5e7386db9e04
tiaow <3696812003@qq.com>
2025-01-20 17:22:47 +0800

    Add files via upload

--- 加班.lua
+++ 加班.lua
@@ -13,17 +13,6 @@ local bin = creds:section("玩家信息",true)
   
     bin:Label("你的用户名:"..game.Players.LocalPlayer.Character.Name)
     bin:Label("你的注入器:"..identifyexecutor())
-    bin:Label("你的账号注册时间:"..game:GetService.("Players").LocalPlayer.AccountAge
-idenifyexecutor())
-    bin:Label("服务器ID"game.PlaceId)
--- 服务器ID
-    bin:Label("服务器名字"game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
--- 服务器名字
-    bin:Label("用户名ID")game:GetService("Players").LocalPlayer.UserId
-    game:GetService("Players").LocalPlayer.CharacterAppearanceId)
--- 用户ID，注册时分配的，不可变
-    bin:Label("客户端ID")game:GetService("RbxAnalyticsService"):GetClientd())
--- 客户端ID，删除再下载游戏就会变
     local bin = creds:section("作者信息",true)
     bin:Label("作者:条纹大地")
     bin:Label("缝合脚本")
