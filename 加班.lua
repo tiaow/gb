f875ea9934957acb8fdd85778f05d6b3a8296a49
tiaow <3696812003@qq.com>
2025-01-30 21:40:39 +0800

    Add files via upload

--- 加班.lua
+++ 加班.lua
@@ -2,23 +2,21 @@ function Notify(Title1, Text1, Icon1)
   game:GetService("StarterGui"):SetCore("SendNotification", {    Title = Title1,    Text = Text1,    Icon = Icon1})
   end
   Notify("条脚本", "作者条纹", "rbxassetid://17360377302", 3)
-  wait(0.5)
+  wait(1)
   Notify("脚本开启成功", "祝你玩的开心","rbxassetid://17360377302",3)
-  wait(0.5)
+  wait(1)
   Notify("打不开的话", "说明在更新","rbxassetid://17360377302",3)
-  wait(0.5)
+  wait(1)
   Notify("启动成功", "祝你玩的开心","rbxassetid://17360377302",3)
   wait(1)
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/nBq2D86q"))()
-local window = library:new("逆天了，老弟")
-
-local creds = window:Tab("信息",'16060333448')
+local window = library:new("条脚本")
+local creds = window:Tab("信息",'106133116600295')
     local bin = creds:section("作者信息",true)
     bin:Label("作者:条纹大地")
     bin:Label("缝合脚本")
     bin:Label("QQ:1023929190")
 local credits = creds:section("关闭",true)
-
 credits:Toggle("脚本框架变小一点", "", false, function(state)
         if state then
         game:GetService("CoreGui")["frosty"].Main.Style = "DropShadow"
@@ -29,31 +27,54 @@ credits:Toggle("脚本框架变小一点", "", false, function(state)
     credits:Button("关闭脚本",function()
         game:GetService("CoreGui")["frosty"]:Destroy()
     end)
-local creds = window:Tab("通用",'16060333448')
+local creds = window:Tab("通用",'106133116600295')
 
-local credits = creds:section("内容",true)                                                           
+local credits = creds:section("内容",true)      
+    credits:Slider("步行速度!", "WalkSpeed", game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, 16, 1000, false, function(Speed)
+  spawn(function() while task.wait() do game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Speed end end)
+end)                                          
     credits:Slider("跳跃高度!", "JumpPower", game.Players.LocalPlayer.Character.Humanoid.JumpPower, 50, 1000, false, function(Jump)
   spawn(function() while task.wait() do game.Players.LocalPlayer.Character.Humanoid.JumpPower = Jump end end)
 end)
-    credits:Slider("步行速度!", "WalkSpeed", game.Players.LocalPlayer.Character.Humanoid.WalkSpeed, 16, 1000, false, function(Speed)
-  spawn(function() while task.wait() do game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Speed end end)
+credits:Slider('设置重力（正常196.2）', 'Sliderflag', 196.2, 0.1, 1000,false, function(Value)
+    game.Workspace.Gravity = Value
 end)
+    credits:Slider('缩放距离', 'ZOOOOOM OUT!',  128, 128, 200000,false, function(value)
+    game:GetService("Players").LocalPlayer.CameraMaxZoomDistance = value
+    end)
+    credits:Slider(
+    "血量",
+    "",
+    100,
+    1,
+    999999999,
+    false,
+    function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.Health = Value
+    end
+)
     credits:Toggle("穿墙", "NoClip", false, function(NC)
   local Workspace = game:GetService("Workspace") local Players = game:GetService("Players") if NC then Clipon = true else Clipon = false end Stepped = game:GetService("RunService").Stepped:Connect(function() if not Clipon == false then for a, b in pairs(Workspace:GetChildren()) do if b.Name == Players.LocalPlayer.Name then for i, v in pairs(Workspace[Players.LocalPlayer.Name]:GetChildren()) do if v:IsA("BasePart") then v.CanCollide = false end end end end else Stepped:Disconnect() end end)
 end)
-
-   credits:Button("隐身道具", function()
-  loadstring(game:HttpGet("https://gist.githubusercontent.com/skid123skidlol/cd0d2dce51b3f20ad1aac941da06a1a1/raw/f58b98cce7d51e53ade94e7bb460e4f24fb7e0ff/%257BFE%257D%2520Invisible%2520Tool%2520(can%2520hold%2520tools)",true))()
-end)                                                                                            
+credits:Toggle("夜视", "Light", false, function(Light)
+  spawn(function() while task.wait() do if Light then game.Lighting.Ambient = Color3.new(1, 1, 1) else game.Lighting.Ambient = Color3.new(0, 0, 0) end end end)
+end)  
+    credits:Toggle("人物显示", "RWXS", false, function(RWXS)    getgenv().enabled = RWXS getgenv().filluseteamcolor = true getgenv().outlineuseteamcolor = true getgenv().fillcolor = Color3.new(1, 0, 0) getgenv().outlinecolor = Color3.new(1, 1, 1) getgenv().filltrans = 0.5 getgenv().outlinetrans = 0.5 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()end) 
+    credits:Button("无敌可能不适用",function()
+     loadstring(game:HttpGet('https://pastebin.com/raw/H3RLCWWZ'))()
+  	end)                                                                                
    credits:Button("飞行v3",function()
   loadstring(game:HttpGet'https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt')()
-end)                                                                                            
+end)                      
+credits:Button("隐身道具", function()
+  loadstring(game:HttpGet("https://gist.githubusercontent.com/skid123skidlol/cd0d2dce51b3f20ad1aac941da06a1a1/raw/f58b98cce7d51e53ade94e7bb460e4f24fb7e0ff/%257BFE%257D%2520Invisible%2520Tool%2520(can%2520hold%2520tools)",true))()
+end)                                                                                
   credits:Button("无限跳跃",function()
   loadstring(game:HttpGet("https://pastebin.com/raw/V5PQy3y0", true))()
 end)  
     credits:Button("反挂机v2",function()  loadstring(game:HttpGet("https://pastebin.com/raw/9fFu43FF"))()end)                 
     credits:Button("透视", function()  local Players = game:GetService("Players"):GetChildren() local RunService = game:GetService("RunService") local highlight = Instance.new("Highlight") highlight.Name = "Highlight" for i, v in pairs(Players) do repeat wait() until v.Character if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then local highlightClone = highlight:Clone() highlightClone.Adornee = v.Character highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart") highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop highlightClone.Name = "Highlight" end end game.Players.PlayerAdded:Connect(function(player) repeat wait() until player.Character if not player.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then local highlightClone = highlight:Clone() highlightClone.Adornee = player.Character highlightClone.Parent = player.Character:FindFirstChild("HumanoidRootPart") highlightClone.Name = "Highlight" end end) game.Players.PlayerRemoving:Connect(function(playerRemoved) playerRemoved.Character:FindFirstChild("HumanoidRootPart").Highlight:Destroy() end) RunService.Heartbeat:Connect(function() for i, v in pairs(Players) do repeat wait() until v.Character if not v.Character:FindFirstChild("HumanoidRootPart"):FindFirstChild("Highlight") then local highlightClone = highlight:Clone() highlightClone.Adornee = v.Character highlightClone.Parent = v.Character:FindFirstChild("HumanoidRootPart") highlightClone.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop highlightClone.Name = "Highlight" task.wait() end end end)end)       
-    credits:Toggle("人物显示", "RWXS", false, function(RWXS)    getgenv().enabled = RWXS getgenv().filluseteamcolor = true getgenv().outlineuseteamcolor = true getgenv().fillcolor = Color3.new(1, 0, 0) getgenv().outlinecolor = Color3.new(1, 1, 1) getgenv().filltrans = 0.5 getgenv().outlinetrans = 0.5 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Highlight-ESP.lua"))()end)                                
+                              
     credits:Button("爬墙",function()
   loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))()
 end)                                                                                            credits:Button("立即死亡",function()
@@ -61,25 +82,6 @@ end)                                                                            
 end)
     credits:Button("复制当前位置", function()
     setclipboard('game:GetService("Players").LocalPlayer.Character:MoveTo(Vector3.new('..tostring(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position)..'))')                       end)
-     local credits = creds:section("抓包工具",true)
-    credits:Button("spy", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/Script-Tutorial/refs/heads/main/Spy.lua"))()
-    end)
-    credits:Button("Dex", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/Script-Tutorial/refs/heads/main/dex.lua"))()
-    end)
-local creds = window:Tab("通用2",'16060333448')
-local credits = creds:section("内容",true)      
-credits:Toggle("夜视", "Light", false, function(Light)
-  spawn(function() while task.wait() do if Light then game.Lighting.Ambient = Color3.new(1, 1, 1) else game.Lighting.Ambient = Color3.new(0, 0, 0) end end end)
-end)
-
-    credits:Slider('设置重力（正常196.2）', 'Sliderflag', 196.2, 0.1, 1000,false, function(Value)
-    game.Workspace.Gravity = Value
-end)
-    credits:Slider('缩放距离', 'ZOOOOOM OUT!',  128, 128, 200000,false, function(value)
-    game:GetService("Players").LocalPlayer.CameraMaxZoomDistance = value
-    end)
     credits:Button("踏空行走", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Float'))()
     end)
@@ -94,17 +96,25 @@ end)
     credits:Button("甩人",function()
     loadstring(game:HttpGet("https://pastebin.com/raw/zqyDSUWX"))()
 end)
-    credits:Button("iw指令", function()  loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()end)
+    credits:Button("iw指令", function()  loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    end)
     
-
-   local creds = window:Tab("传送",'16060333448')                  
+local creds = window:Tab("抓包",'106133116600295')
+local credits = creds:section("工具",true)
+    credits:Button("spy", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/Script-Tutorial/refs/heads/main/Spy.lua"))()
+    end)
+    credits:Button("Dex", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/Script-Tutorial/refs/heads/main/dex.lua"))()
+    end)
+    
+   local creds = window:Tab("传送",'106133116600295')                  
 local credits = creds:section("传送功能",true)
 getgenv().ED_AntiKick = {
 	Enabled = true, -- Set to false if you want to disable the Anti-Kick.
 	SendNotifications = true, -- Set to true if you want to get notified for every event
 	CheckCaller = true -- Set to true if you want to disable kicking by other executed scripts
 }
-
 local dropdown = {}
 local playernamedied = ""
 local teleportConnection
@@ -207,7 +217,7 @@ credits:Toggle("循环传送玩家", "Toggle", false, function(Value)
         Notify("提示", "已停止循环传送玩家", "rbxassetid://", 5)
     end
 end)
-local creds = window:Tab("伐木大亨2",'16060333448')
+local creds = window:Tab("伐木大亨2",'106133116600295')
 local credits = creds:section("伐木大亨",true)
   credits:Button("LuaWareL", function()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/frencaliber/LuaWareLoader.lw/main/luawareloader.wtf",true))()
@@ -215,7 +225,7 @@ local credits = creds:section("伐木大亨",true)
    credits:Button("伐木大亨脚本", function()
    loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Kavo-Ui/main/%E4%BC%90%E6%9C%A8%E5%A4%A7%E4%BA%A82.lua", true))()
    end)
-   local creds = window:Tab("Doors",'16060333448')
+   local creds = window:Tab("Doors",'106133116600295')
 local credits = creds:section("Doors脚本",true)
     credits:Button("Ms", function()
     getgenv().Spy="mspaint" loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoXuAnZang/XKscript/refs/heads/main/DOORS.txt"))()
@@ -226,7 +236,7 @@ local credits = creds:section("Doors脚本",true)
     credits:Button("Doors硬核模式(仅自己可见)", function()
 loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\104\117\121\104\111\97\110\112\104\117\99\47\103\102\47\114\101\102\115\47\104\101\97\100\115\47\109\97\105\110\47\104\99\104\102\99\103\100\99\121\102\103\102\34\41\41\40\41")()
     end)
-    local creds = window:Tab("巴掌",'16060333448')
+    local creds = window:Tab("巴掌",'106133116600295')
     local credits = creds:section("巴掌",true)
     credits:Button("巴掌", function()
     loadstring(game:HttpGet'https://raw.githubusercontent.com/Dusty1234567890/NewGloves/refs/heads/main/Clock')()
@@ -287,7 +297,7 @@ loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\
    credits:Button("终点", function()
    game:GetService("Players").LocalPlayer.Character:MoveTo(Vector3.new(58.005775451660156, 44.20683670043945, 175.5221710205078))
    end)
-   local creds = window:Tab("巴掌2",'16060333448')
+   local creds = window:Tab("巴掌2",'106133116600295')
     local credits = creds:section("防",false)
     credits:Toggle("反板砖","Toggle", false, function(Value)
 AntiBrick = Value
@@ -595,7 +605,7 @@ end
 end
 end)
 
- local creds = window:Tab("力量传奇",'16060333448')
+ local creds = window:Tab("力量传奇",'106133116600295')
  local credits = creds:section("力量传奇脚本",true)    
  credits:Button("力量传奇",function()    loadstring(game:HttpGet('https://raw.githubusercontent.com/jynzl/main/main/Musclas%20Legenos.lua'))()end)
  
@@ -653,7 +663,7 @@ end)
 credits:Button("丛林岛", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-8793.79296875, 23.214065551757812, 2391.373046875)
 end)
- local creds = window:Tab("GB",'16060333448')local credits = creds:section("内容",true)
+ local creds = window:Tab("GB",'106133116600295')local credits = creds:section("内容",true)
  credits:Button("内脏与黑火药脚本", function()
  loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\34\104\116\116\112\115\58\47\47\102\114\101\101\110\111\116\101\46\98\105\122\47\114\97\119\47\109\117\122\110\104\101\114\104\114\117\34\41\44\116\114\117\101\41\41\40\41\10")()end)
  credits:Button("清风GB", function()
