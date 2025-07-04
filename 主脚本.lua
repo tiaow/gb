c60b16e5953d942fb2ce220c19d7100f140de0d0
tiaow <3696812003@qq.com>
2025-07-04 18:22:54 +0800

    更新 主脚本.lua

--- 主脚本.lua
+++ 主脚本.lua
@@ -1825,7 +1825,32 @@ local args = {
 	vector.create(0.5085581541061401, -0.18179628252983093, -0.8416168093681335)
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemoteEvent"):WaitForChild("SwingMelee"):FireServer(unpack(args))
-  task.wait(0.1)
+ local args = {
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("Shovel"),
+	1751615072.037301,
+	vector.create(0.5085581541061401, -0.18179628252983093, -0.8416168093681335)
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemoteEvent"):WaitForChild("SwingMelee"):FireServer(unpack(args))
+local args = {
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("Shovel"),
+	1751615072.037301,
+	vector.create(0.5085581541061401, -0.18179628252983093, -0.8416168093681335)
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemoteEvent"):WaitForChild("SwingMelee"):FireServer(unpack(args))
+local args = {
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("Shovel"),
+	1751615072.037301,
+	vector.create(0.5085581541061401, -0.18179628252983093, -0.8416168093681335)
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemoteEvent"):WaitForChild("SwingMelee"):FireServer(unpack(args))
+local args = {
+	game:GetService("Players").LocalPlayer.Character:WaitForChild("Shovel"),
+	1751615072.037301,
+	vector.create(0.5085581541061401, -0.18179628252983093, -0.8416168093681335)
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemoteEvent"):WaitForChild("SwingMelee"):FireServer(unpack(args))
+
+ task.wait(0.1)
 end
 end)
 
