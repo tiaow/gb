110cc850925c92975241689df287e5b836c9c206
tiaow <3696812003@qq.com>
2025-07-26 17:35:56 +0800

    更新 tiaov2.lua

--- tiaov2.lua
+++ tiaov2.lua
@@ -1,3 +1,4 @@
+
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
  local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
  wait(0.5) 
@@ -16,6 +17,15 @@ if success then
      {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
      {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
  ) 
+if game.Player.Name = Gary_Xi666 then
+local Fg = true
+while Fg do
+print("1")
+task.wait(0.01)
+else
+
+end
+
 else
    Notification:Notify( 
      {Title = "提示", Description = "更新中"}, 
