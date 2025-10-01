8c61728eed85a2f8352501c8920be6e039f3c0ee
tiaow <3696812003@qq.com>
2025-10-01 11:05:21 +0800

    更新 tiaov2.lua

--- tiaov2.lua
+++ tiaov2.lua
@@ -11,22 +11,7 @@ local success, result = pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tiaow/gb/refs/heads/main/%E4%B8%BB%E8%84%9A%E6%9C%AC.lua"))()
 end)
 if success then
-    wait(0.8)
-    Notification:Notify( 
-     {Title = "更新完毕", Description = "祝你玩的开心"}, 
-     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
-     {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
- ) 
-if game.Players.LocalPlayer.Name == "Gary_Xi666" then
-local Fg = true
-while Fg do
-print("1")
-task.wait(0.01)
-end
-else
-print("done")
-end
-
+    
 else
    Notification:Notify( 
      {Title = "提示", Description = "更新中"}, 
