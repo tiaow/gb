b94ddf4c45f5247fa3ab1f27125eb93a6ddd33c8
tiaow <3696812003@qq.com>
2025-10-31 11:12:10 +0800

    更新 tiaov2.lua

--- tiaov2.lua
+++ tiaov2.lua
@@ -1,24 +1,8 @@
 
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
  local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
- wait(0.5) 
- Notification:Notify( 
-     {Title = "提示", Description = "检测更新中"}, 
-     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
-     {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
- ) 
-local success, result = pcall(function()
-    
-if success then
      Notification:Notify( 
      {Title = "停更", Description = "条脚本已关闭(再见)"}, 
      {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
      {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
  ) 
-else
-   Notification:Notify( 
-     {Title = "提示", Description = "更新中"}, 
-     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
-     {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
- ) 
-end
