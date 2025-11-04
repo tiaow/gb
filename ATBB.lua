7b265552c8dfe7c03a1f6a64a646217e6cbed4b0
tiaow <3696812003@qq.com>
2025-11-04 18:19:29 +0800

    更新 ATBB.lua

--- ATBB.lua
+++ ATBB.lua
@@ -48,7 +48,7 @@ end
 
 local Window = WindUI:CreateWindow({
     Title = "战斗砖脚本",
-    Icon = "rbxassetid://140160373995288",
+    Icon = "rbxassetid://92667274604625",
     Author = "作者：条纹",
     Folder = "WindUI_Example",
     Size = UDim2.fromOffset(580, 490),
