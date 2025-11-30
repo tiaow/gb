180de1c2edaf05a91a0fe726b4f698ac4711294e
tiaow <3696812003@qq.com>
2025-11-30 12:30:56 +0800

    更新 tiaov2.lua

--- tiaov2.lua
+++ tiaov2.lua
@@ -2,7 +2,8 @@
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
  local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
      Notification:Notify( 
-     {Title = "停更和关闭", Description = "条脚本已关闭(再见)"}, 
+     {Title = "条脚本提示", Description = "正在检测是否支持该服务器"}, 
      {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"}, 
      {Image = "http://www.roblox.com/asset/?id=4483345998", ImageColor = Color3.fromRGB(255, 84, 84)} 
  ) 
+
