e0f41195cdf85a8463f71bbf127e329ae7bd1d3a
tiaow <3696812003@qq.com>
2026-02-27 01:23:35 +0800

    创建 my GUi.lua

---
+++ my GUi.lua
@@ -0,0 +1,131 @@
+--[=[
+ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
+88      88    88    88            odD'      88      88    88 88ooo88 
+88  ooo 88    88    88          .88'        88      88    88 88~~~88 
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+ Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+
+designed using localmaze gui creator
+]=]
+
+-- Instances: 14 | Scripts: 1 | Modules: 0 | Tags: 0
+local LMG2L = {};
+
+-- Players.yfgg237.PlayerGui.ScreenGui
+LMG2L["ScreenGui_1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+LMG2L["ScreenGui_1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel
+LMG2L["ImageLabel_2"] = Instance.new("ImageLabel", LMG2L["ScreenGui_1"]);
+LMG2L["ImageLabel_2"]["Active"] = true;
+LMG2L["ImageLabel_2"]["BorderSizePixel"] = 0;
+LMG2L["ImageLabel_2"]["ScaleType"] = Enum.ScaleType.Fit;
+LMG2L["ImageLabel_2"]["BackgroundColor3"] = Color3.fromRGB(55, 55, 55);
+LMG2L["ImageLabel_2"]["ResampleMode"] = Enum.ResamplerMode.Pixelated;
+LMG2L["ImageLabel_2"]["AutomaticSize"] = Enum.AutomaticSize.XY;
+LMG2L["ImageLabel_2"]["Size"] = UDim2.new(0, 190, 0, 238);
+LMG2L["ImageLabel_2"]["ClipsDescendants"] = true;
+LMG2L["ImageLabel_2"]["BackgroundTransparency"] = 0.09;
+LMG2L["ImageLabel_2"]["Selectable"] = true;
+LMG2L["ImageLabel_2"]["Position"] = UDim2.new(0, 86, 0, 98);
+LMG2L["ImageLabel_2"]["SelectionGroup"] = true;
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.UICorner
+LMG2L["UICorner_3"] = Instance.new("UICorner", LMG2L["ImageLabel_2"]);
+
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.UIListLayout
+LMG2L["UIListLayout_4"] = Instance.new("UIListLayout", LMG2L["ImageLabel_2"]);
+LMG2L["UIListLayout_4"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+LMG2L["UIListLayout_4"]["ItemLineAlignment"] = Enum.ItemLineAlignment.Center;
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel
+LMG2L["ImageLabel_5"] = Instance.new("ImageLabel", LMG2L["ImageLabel_2"]);
+LMG2L["ImageLabel_5"]["Active"] = true;
+LMG2L["ImageLabel_5"]["BorderSizePixel"] = 0;
+LMG2L["ImageLabel_5"]["SelectionOrder"] = 99;
+LMG2L["ImageLabel_5"]["BackgroundColor3"] = Color3.fromRGB(86, 86, 86);
+LMG2L["ImageLabel_5"]["Size"] = UDim2.new(0, 172, 0, 208);
+LMG2L["ImageLabel_5"]["BackgroundTransparency"] = 0.01;
+LMG2L["ImageLabel_5"]["Position"] = UDim2.new(0, 8, 0, 14);
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.UICorner
+LMG2L["UICorner_6"] = Instance.new("UICorner", LMG2L["ImageLabel_5"]);
+
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame
+LMG2L["ScrollingFrame_7"] = Instance.new("ScrollingFrame", LMG2L["ImageLabel_5"]);
+LMG2L["ScrollingFrame_7"]["BorderSizePixel"] = 0;
+LMG2L["ScrollingFrame_7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["ScrollingFrame_7"]["Size"] = UDim2.new(0, 170, 0, 136);
+LMG2L["ScrollingFrame_7"]["Position"] = UDim2.new(0, 2, 0, 68);
+LMG2L["ScrollingFrame_7"]["ScrollBarThickness"] = 0;
+LMG2L["ScrollingFrame_7"]["BackgroundTransparency"] = 1;
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame.UICorner
+LMG2L["UICorner_8"] = Instance.new("UICorner", LMG2L["ScrollingFrame_7"]);
+
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame.UIListLayout
+LMG2L["UIListLayout_9"] = Instance.new("UIListLayout", LMG2L["ScrollingFrame_7"]);
+LMG2L["UIListLayout_9"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+LMG2L["UIListLayout_9"]["ItemLineAlignment"] = Enum.ItemLineAlignment.Center;
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame.TextButton
+LMG2L["TextButton_a"] = Instance.new("TextButton", LMG2L["ScrollingFrame_7"]);
+LMG2L["TextButton_a"]["Active"] = false;
+LMG2L["TextButton_a"]["BorderSizePixel"] = 0;
+LMG2L["TextButton_a"]["TextSize"] = 9;
+LMG2L["TextButton_a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton_a"]["AutomaticSize"] = Enum.AutomaticSize.XY;
+LMG2L["TextButton_a"]["Size"] = UDim2.new(0, 166, 0, 52);
+LMG2L["TextButton_a"]["ClipsDescendants"] = true;
+LMG2L["TextButton_a"]["Position"] = UDim2.new(0, 2, 0, 56);
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame.TextButton.UICorner
+LMG2L["UICorner_b"] = Instance.new("UICorner", LMG2L["TextButton_a"]);
+
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame.TextButton.LocalScript
+LMG2L["LocalScript_c"] = Instance.new("LocalScript", LMG2L["TextButton_a"]);
+
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.TextLabel
+LMG2L["TextLabel_d"] = Instance.new("TextLabel", LMG2L["ImageLabel_5"]);
+LMG2L["TextLabel_d"]["BorderSizePixel"] = 0;
+LMG2L["TextLabel_d"]["TextSize"] = 32;
+LMG2L["TextLabel_d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextLabel_d"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+LMG2L["TextLabel_d"]["BackgroundTransparency"] = 1;
+LMG2L["TextLabel_d"]["Size"] = UDim2.new(0, 160, 0, 66);
+LMG2L["TextLabel_d"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+LMG2L["TextLabel_d"]["Text"] = [[XXXXX]];
+LMG2L["TextLabel_d"]["Position"] = UDim2.new(0, 4, 0, 0);
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.UIDragDetector
+LMG2L["UIDragDetector_e"] = Instance.new("UIDragDetector", LMG2L["ImageLabel_2"]);
+
+
+
+-- Players.yfgg237.PlayerGui.ScreenGui.ImageLabel.ImageLabel.ScrollingFrame.TextButton.LocalScript
+local function C_c()
+	local script = LMG2L["LocalScript_c"];
+	
+end;
+task.spawn(C_c);
+
+return LMG2L["ScreenGui_1"], require;
