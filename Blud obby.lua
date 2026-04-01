db45b6788da8c4e85f32ad607b8d804d46535712
tiaow <3696812003@qq.com>
2026-04-01 19:47:31 +0800

    更新 Blud obby.lua

--- Blud obby.lua
+++ Blud obby.lua
@@ -1,6 +1,10 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tiaow/gb/refs/heads/main/%E5%BD%A9%E8%89%B2UI.lua"))()
-local window = library:new("TW脚本")
-local creds = window:Tab("信息",'106133116600295')
+local window = library:new("Blud obby")
+local creds = window:Tab("信息",'')
+local Pl = window:Tab("玩家",'')
+local b41 = window:Tab("41关",'')
+local b46 = window:Tab("46关",'')
+
     local bin = creds:section("作者信息",true)
     bin:Label("作者:条纹")
     bin:Label("是否被拉入黑名单：")
@@ -12,8 +16,7 @@ local ban = 9
 task.wait(1)
 end
 
-local P = creds:section("玩家",true)
+local P = Pl:section("玩家",true)
 
-
-local b41 = creds:section("41关",true)
-local b46 = creds:section("46关",true)
+local b41 = b41:section("41关",true)
+local b46 = b46:section("46关",true)
