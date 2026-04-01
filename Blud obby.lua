323908661de1aa5deb19dd8a2c25ce84292ac6ee
tiaow <3696812003@qq.com>
2026-04-01 19:43:15 +0800

    创建 Blud obby.lua

---
+++ Blud obby.lua
@@ -0,0 +1,19 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tiaow/gb/refs/heads/main/%E5%BD%A9%E8%89%B2UI.lua"))()
+local window = library:new("TW脚本")
+local creds = window:Tab("信息",'106133116600295')
+    local bin = creds:section("作者信息",true)
+    bin:Label("作者:条纹")
+    bin:Label("是否被拉入黑名单：")
+    bin:Label("用此脚本请观看视频使用")
+    bin:Label("拉黑名单别找我")
+     
+while true do
+local ban = 9
+task.wait(1)
+end
+
+local P = creds:section("玩家",true)
+
+
+local b41 = creds:section("41关",true)
+local b46 = creds:section("46关",true)
